b17eaf28bb5d3cdd58c19928c8165957b3f7501a
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2022-01-31 21:59:45 +0530

    Create prettier.config.js

---
+++ config/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+}
